362a0a8ab4048ab543ae3e2861544e8749eadb28
Cristian Brinza <cristian.brinza@isa.utm.md>
2025-06-04 11:19:31 +0300

    Removes unused state variables
    Eliminates unnecessary selected type state hooks, simplifying the
    component's state management. Adjusts stroke properties in SVG
    parameters to use camelCase, following JSX conventions.

--- client/src/pages/inventory/Inventory.tsx
+++ client/src/pages/inventory/Inventory.tsx
@@ -43,8 +43,6 @@ const Inventory: React.FC = () => {
 
   // state pentru listele de tipuri și subtips-uri
   const [types, setTypes] = useState<TypeItem[]>([]);
-  const [selectedTypeForAdd, setSelectedTypeForAdd] = useState<string>('');
-  const [selectedTypeForEdit, setSelectedTypeForEdit] = useState<string>('');
 
   const [addmenu, setAddmenu] = useState(false);
 
@@ -131,7 +129,7 @@ const Inventory: React.FC = () => {
     setEditQuantity(item.quantity);
     setEditType(item.type);
     setEditSubType(item.sub_type);
-    setSelectedTypeForEdit(item.type);
+    // Am eliminat referința la selectedTypeForEdit, deoarece nu era folosit
   };
 
   // Confirmă modificare quantity, type și sub_type
@@ -235,7 +233,6 @@ const Inventory: React.FC = () => {
     <div className={styles.inventoryContainer}>
       <header className={styles.inventoryHeader}>
         <h1>
-          {' '}
           <a href="/">← Back</a>
           <br />
           Inventory
@@ -309,7 +306,6 @@ const Inventory: React.FC = () => {
       {renderFilterButtons()}
 
       <section className={styles.inventoryList}>
-        {/*<h2>Lista itemilor</h2>*/}
         {loading && <p>Se încarcă...</p>}
         {error && <p className={styles.error}>{error}</p>}
         {!loading && items.length === 0 && <p>Nu există itemi.</p>}
@@ -360,9 +356,9 @@ const Inventory: React.FC = () => {
                   <path
                     d="M4 7H20M10 11V17M14 11V17M5 7L6 19C6 19.5304 6.21071 20.0391 6.58579 20.4142C6.96086 20.7893 7.46957 21 8 21H16C16.5304 21 17.0391 20.7893 17.4142 20.4142C17.7893 20.0391 18 19.5304 18 19L19 7M9 7V4C9 3.73478 9.10536 3.48043 9.29289 3.29289C9.48043 3.10536 9.73478 3 10 3H14C14.2652 3 14.5196 3.10536 14.7071 3.29289C14.8946 3.48043 15 3.73478 15 4V7"
                     stroke="white"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                   />
                 </svg>
               </button>
